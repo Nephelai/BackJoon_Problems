19150c200304201bf14563af6b78e6ffe2476f23
Cheshire <jaeho406@gmail.com>
2018-09-20 17:44:33 +0900

    Constitute Stack & Queue But Dont know where is wrong..

--- BackJoon_Problems/No_1260.cpp
+++ BackJoon_Problems/No_1260.cpp
@@ -1,66 +1,124 @@
-// DFS와 BFS
-// 4:55 PM 9/19/2018
-
-
-// DFS - stack.
-// BFS - queue.
-
 #include <iostream>
-#include <vector>
 using namespace std;
 
-struct Node {
-	int val;
-	vector<int> connected;
+class Stack {
+public:
+	void Push(int n);
+	int Pop();
+	bool isEmpty();
+
+private:
+	int top = -1;
+	int data[1024];
+};
+
+void Stack::Push(int n) {
+	top++;
+	data[top] = n;
+}
+
+int Stack::Pop() {
+	if (!isEmpty()) {
+		int tmp = data[top];
+		top--;
+		return tmp;
+	}
+	else
+		return 0;
+}
+
+bool Stack::isEmpty() {
+	if (top == -1) return true;
+	else return false;
+}
+
+class Queue{
+public:
+	void Push(int n);
+	int Pop();
+	bool isEmpty();
+
+private:
+	int front = 0;
+	int end = 0;
+	int data[1024];
+};
+
+void Queue::Push(int n) {
+	data[end] = n;
+	end++;
+}
+
+int Queue::Pop() {
+	if (!isEmpty()) {
+		int tmp = data[front];
+		front++;
+		return tmp;
+	}
+	else
+		return 0;
+}
+
+bool Queue::isEmpty() {
+	if (front == end) return true;
+	else return false;
+}
+
+
+struct AdjMatrix {
+	bool connected[1024] = { false, };
 	bool visited = false;
 };
 
-Node nodes[1024];
+AdjMatrix map[1024];
+int vertexs, edges, startNode;
+int num1, num2;
 
-int cnt_node, cnt_edge, startNode;
-int tmp1, tmp2;
 
-void DFS(int n) {
-	printf("%d ", n);
-	nodes[n].visited = true;
-	vector<int>::iterator iter = nodes[n].connected.begin();
-	while (iter != nodes[n].connected.end()) {
-		if(!nodes[*iter].visited) DFS(*iter);
-		iter++;
+// Stack 만으로 DFS 구성이 가능한가?
+// 거꾸로 넣으면..?
+void DFS(int s) {
+	Stack stack;
+	stack.Push(s);
+	int tmp = -1;
+	while (!stack.isEmpty()) {
+		tmp = stack.Pop();
+		if (map[tmp].visited) continue;
+		printf("%d ", tmp);
+		map[tmp].visited = true;
+		for (int i = vertexs; i > 0; i--) {
+			if (map[tmp].connected[i] && !map[i].visited) stack.Push(i);
+		}
 	}
 }
 
-void BFS(int n) {
-	nodes[n].visited = true;
-	vector<int>::iterator iter = nodes[n].connected.begin();
-	while (iter != nodes[n].connected.end()) {
-		if (!nodes[*iter].visited) printf("%d ", *iter);
-		iter++;
-	}
-
-	iter = nodes[n].connected.begin();
-	while (iter != nodes[n].connected.end()) {
-		if (!nodes[*iter].visited) BFS(*iter);
-		iter++;
+void BFS(int s) {
+	Queue queue;
+	queue.Push(s);
+	int tmp = -1;
+	while (!queue.isEmpty()) {
+		tmp = queue.Pop();
+		if (map[tmp].visited) continue;
+		printf("%d ", tmp);
+		map[tmp].visited = true;
+		for (int i = 1; i <= vertexs; i++) {
+			if (map[tmp].connected[i] && !map[i].visited) queue.Push(i);
+		}
 	}
 }
+
 
 int main() {
-
-	scanf("%d %d %d", &cnt_node, &cnt_edge, &startNode);
-	for (int i = 1; i <= cnt_node; i++) nodes[i].val = i;
-
-	for (int i = 0; i < cnt_edge; i++) {
-		scanf("%d %d", &tmp1, &tmp2);
-
-		nodes[tmp1].connected.push_back(tmp2);
-		nodes[tmp2].connected.push_back(tmp1);
+	scanf("%d %d %d", &vertexs, &edges, &startNode);
+	for (int i = 0; i < edges; i++) {
+		scanf("%d %d", &num1, &num2);
+		map[num1].connected[num2] = true;
+		map[num2].connected[num1] = true;
 	}
 
 	DFS(startNode);
-	for (int i = 1; i <= cnt_node; i++) nodes[i].visited = false;
-
-	printf("\n%d ", startNode);
+	for (int i = 1; i <= vertexs; i++) map[i].visited = false;
+	printf("\n");
 	BFS(startNode);
 
 	return 0;
